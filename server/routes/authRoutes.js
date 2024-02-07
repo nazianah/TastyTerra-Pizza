b83cfbc39621d43a88d5295596ead65a0baae68e
nazianah <nazianah.umme@gmail.com>
2024-02-07 14:41:13 +0400

    Update origin URL in authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const { test, registerUser, loginUser, getProfile, logoutUser } = require('../co
 //middleware
 router.use(cors(
     {
-        origin: 'http://localhost:3000',
+        origin: 'https://tasty-terra-pizza.vercel.app/',
         credentials: true
     }
 ));
