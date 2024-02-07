7a89aa409e7271f15b8e08337258d464156677d9
nazianah <nazianah.umme@gmail.com>
2024-02-07 16:02:22 +0400

    Add session middleware and update auth routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const session = require('express-session');
 const router = express.Router();
 const cors = require('cors');
 const { test, registerUser, loginUser, getProfile, logoutUser } = require('../controllers/authController');
 
-//middleware
+// Middleware
 router.use(cors({
     origin: 'https://tasty-terra-pizza.vercel.app',
     credentials: true
 }));
 
+router.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 3600000 } // Adjust the maxAge as needed
+}));
+
+// Routes
 router.get("/", test);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
