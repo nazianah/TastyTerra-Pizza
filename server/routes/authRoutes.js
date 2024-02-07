d03405f69a6f1154ed33ba50aec6f478bf0e9ac5
nazianah <nazianah.umme@gmail.com>
2024-02-07 16:05:48 +0400

    Refactor authRoutes.js: Update middleware and remove unused code

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,23 +1,14 @@
 const express = require('express');
-const session = require('express-session');
 const router = express.Router();
 const cors = require('cors');
 const { test, registerUser, loginUser, getProfile, logoutUser } = require('../controllers/authController');
 
-// Middleware
+//middleware
 router.use(cors({
     origin: 'https://tasty-terra-pizza.vercel.app',
     credentials: true
 }));
 
-router.use(session({
-    secret: 'your-secret-key',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false, maxAge: 3600000 } // Adjust the maxAge as needed
-}));
-
-// Routes
 router.get("/", test);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
