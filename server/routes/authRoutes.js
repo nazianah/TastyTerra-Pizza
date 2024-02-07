271a0c51f9a3613279d4581df81e6a0937404206
nazianah <nazianah.umme@gmail.com>
2024-02-07 15:27:54 +0400

    Update API base URL and CORS settings

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const { test, registerUser, loginUser, getProfile, logoutUser } = require('../co
 //middleware
 router.use(cors(
     {
-        origin: 'https://tasty-terra-pizza.vercel.app/',
+        origin: '*',
         credentials: true
     }
 ));
