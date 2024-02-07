377c8a8a6c5cd17bc5c4ef614042df00b54d919e
nazianah <nazianah.umme@gmail.com>
2024-02-07 15:58:01 +0400

    Update authRoutes.js to set CORS origin to 'https://tasty-terra-pizza.vercel.app'

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,22 +3,16 @@ const router = express.Router();
 const cors = require('cors');
 const { test, registerUser, loginUser, getProfile, logoutUser } = require('../controllers/authController');
 
-
 //middleware
-router.use(cors(
-    {
-        origin: '*',
-        
-        credentials: true
-    }
-));
+router.use(cors({
+    origin: 'https://tasty-terra-pizza.vercel.app',
+    credentials: true
+}));
 
-router.get("/", test)
-router.post("/register", registerUser)
-router.post("/login", loginUser )
+router.get("/", test);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.post('/logout', logoutUser);
 router.get('/profile', getProfile);
-
-
 
 module.exports = router;
