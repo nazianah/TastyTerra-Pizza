59792eae5714638a6434f38134c3c84a2495d1e1
nazianah <nazianah.umme@gmail.com>
2024-02-07 13:09:06 +0400

    Fix login redirect and remove page reload

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
       } else {
         setData({});
         navigate("/profile");
-        window.location.reload();
+        // window.location.reload();
         toast.success("Login successful!");
       }
     } catch (error) {
