0538c3aed4f5619218c33c1e1e21d311258fd6ea
nazianah <nazianah.umme@gmail.com>
2024-02-07 12:25:02 +0400

    Update token expiration time in getProfile function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -63,7 +63,7 @@ const loginUser = async (req, res) => {
 const getProfile = (req, res) => {
     const {token} = req.cookies
     if(token) {
-        jwt.verify(token, process.env.JWT_SECRET, {} , (err, user) => {
+        jwt.verify(token, process.env.JWT_SECRET, { expiresIn: '1h' } , (err, user) => {
             if(err) throw err;
             res.json(user)
         })
