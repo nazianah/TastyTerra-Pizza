16bc6a7905bbde423ebe09a3e02b070eeb03073b
nazianah <nazianah.umme@gmail.com>
2024-03-03 17:50:16 +0400

    Refactor routes in Routers.js

--- client/src/routes/Routers.js
+++ client/src/routes/Routers.js
@@ -1,36 +1,3 @@
-// import React from "react";
-// import { Routes, Route, Navigate } from "react-router-dom";
-
-// import Home from "../pages/Home";
-// import AllFoods from "../pages/AllFoods";
-// import FoodDetails from "../pages/FoodDetails";
-// import Cart from "../pages/Cart";
-// import Checkout from "../pages/Checkout";
-// import Contact from "../pages/Contact";
-// import Login from "../pages/Login";
-// import Register from "../pages/Register";
-// // import Profile from "../pages/Dashboard";
-// import Dashboard from "../pages/Dashboard";
-
-// const Routers = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Navigate to="/home" />} />
-//       <Route path="/home" element={<Home />} />
-//       <Route path="/foods" element={<AllFoods />} />
-//       <Route path="/foods/:id" element={<FoodDetails />} />
-//       <Route path="/cart" element={<Cart />} />
-//       <Route path="/checkout" element={<Checkout />} />
-//       <Route path="/login" element={<Login />} />
-//       <Route path="/profile" element={<Dashboard />} />
-//       <Route path="/register" element={<Register />} />
-//       <Route path="/contact" element={<Contact />} />
-//     </Routes>
-//   );
-// };
-
-// export default Routers;
-
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -42,23 +9,8 @@ import Checkout from "../pages/Checkout";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+// import Profile from "../pages/Dashboard";
 import Dashboard from "../pages/Dashboard";
-
-// Define a function to check if the user is logged in
-const isAuthenticated = () => {
-  // Implement your logic here to check if the user is logged in
-  // For example, you can check if there's a token in localStorage or sessionStorage
-  return localStorage.getItem("token") !== null;
-};
-
-// Create a higher-order component to restrict routes
-const PrivateRoute = ({ element, path }) => {
-  return isAuthenticated() ? (
-    <Route path={path} element={element} />
-  ) : (
-    <Navigate to="/login" replace />
-  );
-};
 
 const Routers = () => {
   return (
@@ -70,10 +22,9 @@ const Routers = () => {
       <Route path="/cart" element={<Cart />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Dashboard />} />
       <Route path="/register" element={<Register />} />
       <Route path="/contact" element={<Contact />} />
-      {/* Use the PrivateRoute component for restricted routes */}
-      <PrivateRoute path="/profile" element={<Dashboard />} />
     </Routes>
   );
 };
